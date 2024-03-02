7ed492009d6908efa4c568bb74b538cab77d044c
Ernest103 <ernest0399@icloud.com>
2024-03-02 13:43:46 -0500

    Updated the original script to work as of 3/2/2024

--- delete_mails.py
+++ delete_mails.py
@@ -5,48 +5,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get('https://mail.yahoo.com')
-driver.find_element_by_id('login-username').click()
-driver.find_element_by_id('login-username').clear()
-driver.find_element_by_id('login-username').send_keys("your_yahoo_mail_id")#eg: yohan_saby@yahoo.co.in
-driver.find_element_by_id('login-passwd').click()
-driver.find_element_by_id('login-passwd').clear()
-driver.find_element_by_id('login-passwd').send_keys("your_password")
-driver.find_element_by_id('login-signin').click()
 
+signin_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Sign in')))
+signin_button.click()
 
+# You should interact with elements after the page has loaded, so it's better to wait for the element to be present
+username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-username')))
+# Now you can interact with the element
+username_field.click()
+username_field.clear()
+username_field.send_keys("yourUserName")#eg: yohan_saby@yahoo.co.in
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-signin'))).click()
+
+# You should interact with elements after the page has loaded, so it's better to wait for the element to be present
+passwordField = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-passwd')))
+# Now you can interact with the element
+passwordField.click()
+passwordField.clear()
+passwordField.send_keys("yourPassword")
+
+#Sing In
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-signin'))).click()
 while(True):
-    print 'deleting'
+    print ('deleting')
     
     # I had superslow internet and hence i gave implicit wait 30. You can ofcourse reduce it 
     # but it actually won't matter since its implicit wait.
     driver.implicitly_wait(30)
-    checkbox = driver.find_element_by_css_selector('input[data-action="selectAll-message"]')
+    checkbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[data-test-id="checkbox"]')))
     i = 0
     ''' 
     There are arbitarily 10 attempts made to select the checkbox. 
     The reason it is put in while loop is because sometimes for unknown reasons the checkbox is not selected in one go.
     Also, if there are no more mails to be deleted then the loop might go infinte. Hence, I arbitarily chose 10 attempts
     '''
-    while not checkbox.is_selected():
-        if i == 9:
+    while i < 10:
+        checkbox.click()
+        # Introduce a small delay to allow the checkbox to update its state
+        time.sleep(1)
+        if checkbox.get_attribute("aria-checked") == "true":
             break
-        else:
-            checkbox.click()
-            i +=9
+        i += 1
 
-    print 'is checkbox selected',checkbox.is_selected()
-    if checkbox.is_selected():
+    if checkbox.get_attribute("aria-checked") == "true":
         wait = WebDriverWait(driver, 30)#arbitary wait time
-        delete_button = wait.until(EC.element_to_be_clickable((By.ID,'btn-delete')))
+        delete_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'button[data-test-id="toolbar-delete"]')))
         delete_button.click()
-
-        confirm_button = wait.until(EC.element_to_be_clickable((By.ID,'okModalOverlay')))
-        confirm_button.click()
         
-        time.sleep(10)#sleeping as yahoo mail takes some time if there are lot of messages to be deleted.
-        print 'deleted'
+        time.sleep(3)#sleeping as yahoo mail takes some time if there are lot of messages to be deleted.
+        print ('deleted')
     else:
-        print 'Looks like there are no more mails to delete'
+        print ('Looks like there are no more mails to delete')
         break
